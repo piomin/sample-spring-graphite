d1f6c696b61d7b671a44796e4739aab701ee4b8d
piotr.minkowski <piotr.minkowski@gmail.com>
2017-07-11 14:00:12 +0200

    first service

--- src/main/java/pl/piomin/graphite/Start.java
+++ src/main/java/pl/piomin/graphite/Start.java
@@ -6,21 +6,17 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import pl.piomin.graphite.service.FirstService;
-import pl.piomin.graphite.service.SecondService;
 
 @Component
 public class Start implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired
 	private FirstService service1;
-	@Autowired
-	private SecondService service2;
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 		System.out.println("Hello");
-//		service1.exampleMethod();
-//		service2.exampleMethod();
+		service1.exampleMethod();
 	}
 	
 }
--- src/main/java/pl/piomin/graphite/service/FirstService.java
+++ src/main/java/pl/piomin/graphite/service/FirstService.java
@@ -19,7 +19,7 @@ public class FirstService {
     public void exampleMethod() {
     	Random r = new Random();
     	for (int i = 0; i < 1000000; i++) {
-    		this.gaugeService.submit("hello", r.nextDouble()*100);
+    		this.gaugeService.submit("firstservice", r.nextDouble()*100);
     		try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
