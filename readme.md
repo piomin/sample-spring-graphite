ded6dd5c2ead23f1fcef596e3303c8a702cc6d9a
piomin <piotr.minkowski@gmail.com>
2023-03-24 13:01:11 +0100

    fix errors

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-## Custom metrics visualization with Grafana and InfluxDB
+## Custom metrics visualization with Grafana and InfluxDB [![Twitter](https://img.shields.io/twitter/follow/piotr_minkowski.svg?style=social&logo=twitter&label=Follow%20Me)](https://twitter.com/piotr_minkowski)
+
+[![CircleCI](https://circleci.com/gh/piomin/sample-spring-graphite.svg?style=svg)](https://circleci.com/gh/piomin/sample-spring-microservices-new)
 
 Detailed description can be found here: [Custom metrics visualization with Grafana and InfluxDB](https://piotrminkowski.com/2017/07/13/custom-metrics-visualization-with-grafana-and-influxdb/) 
 
--- src/main/java/pl/piomin/graphite/GraphiteApplication.java
+++ src/main/java/pl/piomin/graphite/GraphiteApplication.java
@@ -13,30 +13,5 @@ public class GraphiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraphiteApplication.class, args);
 	}
-	
-//	@Bean
-//	public MetricsEndpointMetricReader metricsEndpointMetricReader(final MetricsEndpoint metricsEndpoint) {
-//		return new MetricsEndpointMetricReader(metricsEndpoint);
-//	}
 
-//	@Bean
-//	@ExportMetricWriter
-//	GaugeWriter influxMetricsWriter() {
-//		InfluxDB influxDB = InfluxDBFactory.connect("http://192.168.99.100:8086", "root", "root");
-//		String dbName = "grafana";
-//		influxDB.setDatabase(dbName);
-//		influxDB.setRetentionPolicy("one_day");
-//		influxDB.enableBatch(10, 1000, TimeUnit.MILLISECONDS);
-//
-//		return new GaugeWriter() {
-//
-//			@Override
-//			public void set(Metric<?> value) {
-//				Point point = Point.measurement(value.getName()).time(value.getTimestamp().getTime(), TimeUnit.MILLISECONDS)
-//						.addField("value", value.getValue()).build();
-//				influxDB.write(point);
-//				logger.info("write(" + value.getName() + "): " + value.getValue());
-//			}
-//		};
-//	}
 }
